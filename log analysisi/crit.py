64edb6775ad44ff58df1b81e3dd15e9bbac1c75a
Sandhya Anand <124878411+naturatigris@users.noreply.github.com>
2023-02-13 11:19:12 +0530

    Update crit.py

--- log analysisi/crit.py
+++ log analysisi/crit.py
@@ -1,7 +1,7 @@
 from statistics import mode
 import pandas as pd
 import matplotlib.pyplot as plt
-file=open(r"/home/sandhya/.cache/.fr-aZJure/BMC_LOG/crit.log","r")
+file=open(" enter the file location ")
 cnt=0
 c=file.readlines()
 l=[]
